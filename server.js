7c1232f35948cf7e670287d32ebb73011feec917
shun91 <shun9167@gmail.com>
2017-07-21 21:02:03 +0900

    今度こそアクセス制限...

--- server.js
+++ server.js
@@ -12,26 +12,27 @@ const port = process.env.PORT || config.dev.port;
 const app = express();
 
 /** *************************** アクセス制限ここから *****************************/
-// 社内ipならtrueになる
-let isYahooIp = false;
-// 社内ipかの判定
-app.use((req, res, next) => {
-  const yahooIps = [
-    '211.14.8.0/24',
-    '211.14.26.0/23',
-    '211.14.28.0/23',
-    '203.141.54.0/24',
-    '103.2.244.0/22',
-  ];
-  const ip = req.headers['x-forwarded-for'] || req.ip;
-  isYahooIp = rangeCheck.inRange(ip, yahooIps);
-  next();
-});
-// 社外ipの場合はbasic認証
-if (!isYahooIp) {
-  app.all('*', basicAuth(
-    (user, password) => user === 'coeic' && password === 'hackday14',
-  ));
+// 環境変数によって利用する認証方法を切り替え
+if (process.env.AUTH === 'IP') {
+  // 社内ipのみアクセス許可
+  app.use((req, res, next) => {
+    const yahooIps = [
+      '211.14.8.0/24',
+      '211.14.26.0/23',
+      '211.14.28.0/23',
+      '203.141.54.0/24',
+      '103.2.244.0/22',
+    ];
+    const ip = req.headers['x-forwarded-for'] || req.ip;
+    if (rangeCheck.inRange(ip, yahooIps)) {
+      next();
+    } else {
+      res.status(403).send('403 Forbidden.');
+    }
+  });
+} else {
+  // basic認証
+  app.use(basicAuth('coeic', 'hackday14'));
 }
 /** *************************** アクセス制限ここまで *****************************/
 
