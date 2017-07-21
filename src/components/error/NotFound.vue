9a115b30e65b8382d2691b66343080a444e8ff01
shun91 <shun9167@gmail.com>
2017-07-21 21:26:57 +0900

    HTML5 History モード導入

---
+++ src/components/error/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="not-found">
+    Not Found.
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'not-found',
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '@/components/home/Home';
+import NotFound from '@/components/error/NotFound';
 
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'Home',
       component: Home,
+    },
+    {
+      path: '*',
+      component: NotFound,
     },
   ],
 });
