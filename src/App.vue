390554966f83c175bd3d6220557b3cbf3fd79749
Mchisa <chisa@chisa-m.com>
2017-07-22 18:32:50 +0900

    画像追加とCSS適用

--- src/App.vue
+++ src/App.vue
@@ -12,12 +12,26 @@ export default {
 </script>
 
 <style>
+* {
+-webkit-box-sizing: border-box;
+-ms-box-sizing: border-box;
+-moz-box-sizing: border-box;
+-o-box-sizing: border-box;
+box-sizing: border-box;
+}
+
+body {
+  color: #333;
+  background: url('./assets/bg/bg.jpg') no-repeat;
+  background-size: cover;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 40px;
 }
 </style>
