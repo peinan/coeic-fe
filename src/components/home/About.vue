10e1f9188d617641bb5fd7b5f67d1764c36dc6ab
Mchisa <chisa@chisa-m.com>
2017-07-24 02:13:11 +0900

    aboutをつくった

--- src/components/home/About.vue
+++ src/components/home/About.vue
@@ -1,9 +1,10 @@
 <template>
-  <div class="player">
+  <div class="view">
       <div id="black-overlay"></div>
-      <div id="frame-playlist-base">
+      <div id="about-base">
         <img src="../../assets/txt/about.png" width="361" height="344" alt="coeicとは、とは、漫画に楽しみを
 プラスするサービスです。音声の読み上げ機能を搭載しており、今後、次のような機能を実装予定です。感情や場面に応じた音声の生成、話者の性別等による音声の分別、音声による操作（アップロードなど）対応">
+        <p><br><br><router-link :to="{name: 'Upload'}"><img src="../../assets/btn/close.png" width="71" height="17" alt="閉じる"></router-link></p>
       </div>
   </div>
 </template>
@@ -13,27 +14,28 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.player {
+.view {
   margin: 50px 0;
 }
 
-#play {
-  display: inline-block;
-  background: url('../../assets/icn/play.png') no-repeat left center / 35px 35px;
-  margin: 15px 0 0 -20px;
-  padding: 5px 0 0 40px;
-  height: 35px;
-  font-size: 14px;
-}
-
-#frame-playlist-base {
+#about-base {
   position: absolute;
   top: 40px;
   width: 950px;
   margin: 0 auto;
   background: url('../../assets/bg/main-base.png') no-repeat center center;
   background-size: cover;
-  padding: 30px 10px 40px;
+  padding: 60px 10px 40px;
   z-index: 4;
+}
+
+#black-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0,0,0,0.8);
+  z-index: 3;
 }
 </style>
